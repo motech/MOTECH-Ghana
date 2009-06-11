935dbe10dc7511bf3243e2cbd3c5088bf749c162
Matt Blanchette <mblanchette@usm.maine.edu>
2009-06-11 21:16:16 +0000

    Added parameter names in Web Service methods for wsdl/client generation
    git-svn-id: https://motech.rcg.usm.maine.edu/svn/motech/trunk@24 6f28304a-7744-dd4f-b2a8-8a00c5622ee2

--- motech-core/src/main/java/org/motech/service/RegistrarService.java
+++ motech-core/src/main/java/org/motech/service/RegistrarService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import org.motech.ejb.Registrar;
@@ -15,10 +16,19 @@ public class RegistrarService {
 	Registrar registrationBean;
 
 	@WebMethod
-	public void registerMother(String nursePhoneNumber, Date date,
-			String serialId, String name, String community, String location,
-			Integer age, Integer nhis, String phoneNumber, Date dueDate,
-			Integer parity, Integer hemoglobin) {
+	public void registerMother(
+			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
+			@WebParam(name = "date") Date date,
+			@WebParam(name = "serialId") String serialId,
+			@WebParam(name = "name") String name,
+			@WebParam(name = "community") String community,
+			@WebParam(name = "location") String location,
+			@WebParam(name = "age") Integer age,
+			@WebParam(name = "nhis") Integer nhis,
+			@WebParam(name = "phoneNumber") String phoneNumber,
+			@WebParam(name = "dueDate") Date dueDate,
+			@WebParam(name = "parity") Integer parity,
+			@WebParam(name = "hemoglobin") Integer hemoglobin) {
 
 		registrationBean.registerMother(nursePhoneNumber, date, serialId, name,
 				community, location, age, nhis, phoneNumber, dueDate, parity,
@@ -26,33 +36,57 @@ public class RegistrarService {
 	}
 
 	@WebMethod
-	public void registerNurse(String name, String phoneNumber, String clinic) {
+	public void registerNurse(
+			@WebParam(name = "name") String name,
+			@WebParam(name = "phoneNumber") String phoneNumber,
+			@WebParam(name = "clinic") String clinic) {
 
 		registrationBean.registerNurse(name, phoneNumber, clinic);
 	}
 
 	@WebMethod
-	public void registerPatient(String nursePhoneNumber, String serialId,
-			String name, String community, String location, Integer age,
-			String gender, Integer nhis, String phoneNumber) {
+	public void registerPatient(
+			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
+			@WebParam(name = "serialId") String serialId,
+			@WebParam(name = "name") String name,
+			@WebParam(name = "community") String community,
+			@WebParam(name = "location") String location,
+			@WebParam(name = "age") Integer age,
+			@WebParam(name = "gender") String gender,
+			@WebParam(name = "nhis") Integer nhis,
+			@WebParam(name = "phoneNumber") String phoneNumber) {
 
 		registrationBean.registerPatient(nursePhoneNumber, serialId, name,
 				community, location, age, gender, nhis, phoneNumber);
 	}
 
 	@WebMethod
-	public void registerPregnancy(String nursePhoneNumber, Date date,
-			String serialId, Date dueDate, Integer parity, Integer hemoglobin) {
+	public void registerPregnancy(
+			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
+			@WebParam(name = "date") Date date,
+			@WebParam(name = "serialId") String serialId,
+			@WebParam(name = "dueDate") Date dueDate,
+			@WebParam(name = "parity") Integer parity,
+			@WebParam(name = "hemoglobin") Integer hemoglobin) {
 
 		registrationBean.registerPregnancy(nursePhoneNumber, date, serialId,
 				dueDate, parity, hemoglobin);
 	}
 
 	@WebMethod
-	public void recordMaternalVisit(String nursePhoneNumber, Date date,
-			String serialId, Integer tetanus, Integer ipt, Integer itn,
-			Integer visitNumber, Integer onARV, Integer prePMTCT,
-			Integer testPMTCT, Integer postPMTCT, Integer hemoglobinAt36Weeks) {
+	public void recordMaternalVisit(
+			@WebParam(name = "nursePhoneNumber") String nursePhoneNumber,
+			@WebParam(name = "date") Date date,
+			@WebParam(name = "serialId") String serialId,
+			@WebParam(name = "tetanus") Integer tetanus,
+			@WebParam(name = "ipt") Integer ipt,
+			@WebParam(name = "itn") Integer itn,
+			@WebParam(name = "visitNumber") Integer visitNumber,
+			@WebParam(name = "onARV") Integer onARV,
+			@WebParam(name = "prePMTCT") Integer prePMTCT,
+			@WebParam(name = "testPMTCT") Integer testPMTCT,
+			@WebParam(name = "postPMTCT") Integer postPMTCT,
+			@WebParam(name = "hemoglobinAt36Weeks") Integer hemoglobinAt36Weeks) {
 
 		registrationBean.recordMaternalVisit(nursePhoneNumber, date, serialId,
 				tetanus, ipt, itn, visitNumber, onARV, prePMTCT, testPMTCT,
