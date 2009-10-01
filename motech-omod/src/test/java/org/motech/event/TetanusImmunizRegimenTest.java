79581c10da806c9cf468124affcbd786d862c30c
Matt Blanchette <mblanchette@usm.maine.edu>
2009-10-01 18:26:49 +0000

    Refs #88 Added tests for tetanus regimens
    git-svn-id: https://motech.rcg.usm.maine.edu/svn/motech/trunk@250 6f28304a-7744-dd4f-b2a8-8a00c5622ee2

---
+++ motech-omod/src/test/java/org/motech/event/TetanusImmunizRegimenTest.java
@@ -0,0 +1,397 @@
+package org.motech.event;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.motech.event.impl.RegimenImpl;
+import org.motech.event.impl.RegimenStateImpl;
+import org.motech.event.impl.RegimenStateTransitionExpectedNumImpl;
+import org.motech.event.impl.ScheduleMessageCommand;
+import org.motech.messaging.MessageScheduler;
+import org.openmrs.Patient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TetanusImmunizRegimenTest extends TestCase {
+
+	ApplicationContext ctx;
+
+	Patient patient;
+	Integer patientId;
+
+	RegimenImpl regimen;
+	RegimenStateImpl state1;
+	RegimenStateImpl state2;
+	RegimenStateImpl state3;
+	RegimenStateImpl state4;
+	RegimenStateImpl state5;
+	RegimenStateImpl state6;
+	RegimenStateImpl state7;
+
+	MessageScheduler messageScheduler;
+	PatientObsService patientObsService;
+
+	@Override
+	protected void setUp() throws Exception {
+		patientId = 572;
+
+		patient = new Patient();
+		patient.setPatientId(patientId);
+
+		ctx = new ClassPathXmlApplicationContext("tetanus-immuniz-regimen.xml");
+
+		regimen = (RegimenImpl) ctx.getBean("tetanusImmunization");
+
+		state1 = (RegimenStateImpl) ctx.getBean("tetanusState1");
+		state2 = (RegimenStateImpl) ctx.getBean("tetanusState1Reminder1");
+		state3 = (RegimenStateImpl) ctx.getBean("tetanusState1Reminder2");
+		state4 = (RegimenStateImpl) ctx.getBean("tetanusState2");
+		state5 = (RegimenStateImpl) ctx.getBean("tetanusState2Reminder1");
+		state6 = (RegimenStateImpl) ctx.getBean("tetanusState2Reminder2");
+		state7 = (RegimenStateImpl) ctx.getBean("tetanusState3");
+
+		patientObsService = createMock(PatientObsService.class);
+
+		state1.setPatientObsService(patientObsService);
+		for (RegimenStateTransition transition : state1.getTransitions()) {
+			if (transition instanceof RegimenStateTransitionExpectedNumImpl) {
+				((RegimenStateTransitionExpectedNumImpl) transition)
+						.setPatientObsService(patientObsService);
+			}
+		}
+		state2.setPatientObsService(patientObsService);
+		for (RegimenStateTransition transition : state2.getTransitions()) {
+			if (transition instanceof RegimenStateTransitionExpectedNumImpl) {
+				((RegimenStateTransitionExpectedNumImpl) transition)
+						.setPatientObsService(patientObsService);
+			}
+		}
+		state3.setPatientObsService(patientObsService);
+		for (RegimenStateTransition transition : state3.getTransitions()) {
+			if (transition instanceof RegimenStateTransitionExpectedNumImpl) {
+				((RegimenStateTransitionExpectedNumImpl) transition)
+						.setPatientObsService(patientObsService);
+			}
+		}
+		state4.setPatientObsService(patientObsService);
+		for (RegimenStateTransition transition : state4.getTransitions()) {
+			if (transition instanceof RegimenStateTransitionExpectedNumImpl) {
+				((RegimenStateTransitionExpectedNumImpl) transition)
+						.setPatientObsService(patientObsService);
+			}
+		}
+		state5.setPatientObsService(patientObsService);
+		for (RegimenStateTransition transition : state5.getTransitions()) {
+			if (transition instanceof RegimenStateTransitionExpectedNumImpl) {
+				((RegimenStateTransitionExpectedNumImpl) transition)
+						.setPatientObsService(patientObsService);
+			}
+		}
+		state6.setPatientObsService(patientObsService);
+		for (RegimenStateTransition transition : state6.getTransitions()) {
+			if (transition instanceof RegimenStateTransitionExpectedNumImpl) {
+				((RegimenStateTransitionExpectedNumImpl) transition)
+						.setPatientObsService(patientObsService);
+			}
+		}
+
+		messageScheduler = createMock(MessageScheduler.class);
+
+		((ScheduleMessageCommand) state1.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state2.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state3.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state4.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state5.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state6.getCommand())
+				.setMessageScheduler(messageScheduler);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		patient = null;
+		patientId = null;
+
+		regimen = null;
+		state1 = null;
+		state2 = null;
+		state3 = null;
+		state4 = null;
+		state5 = null;
+		state6 = null;
+		state7 = null;
+
+		ctx = null;
+
+		patientObsService = null;
+		messageScheduler = null;
+	}
+
+	public void testState1() {
+		RegimenState expectedState = state1;
+		// No tetanus immunizations, patient registered 0 minutes ago, message
+		// expected in 3 minutes
+		Integer numberOfTetanusObs = 0;
+		Date registrationDate = setPatientRegistration(patient, 0);
+		Date messageDate = getMessageDate(registrationDate, 3);
+
+		String messageKey = ((ScheduleMessageCommand) expectedState
+				.getCommand()).getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) expectedState.getCommand())
+				.getPublicId();
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs);
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState2() {
+		RegimenState expectedState = state2;
+		// No tetanus immunizations, patient registered 4 minutes ago, message
+		// expected 6 minutes after registration
+		Integer numberOfTetanusObs = 0;
+		Date registrationDate = setPatientRegistration(patient, 4);
+		Date messageDate = getMessageDate(registrationDate, 6);
+
+		String messageKey = ((ScheduleMessageCommand) expectedState
+				.getCommand()).getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) expectedState.getCommand())
+				.getPublicId();
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState3() {
+		RegimenState expectedState = state3;
+		// No tetanus immunizations, patient registered 7 minutes ago, message
+		// expected 9 minutes after registration
+		Integer numberOfTetanusObs = 0;
+		Date registrationDate = setPatientRegistration(patient, 7);
+		Date messageDate = getMessageDate(registrationDate, 9);
+
+		String messageKey = ((ScheduleMessageCommand) expectedState
+				.getCommand()).getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) expectedState.getCommand())
+				.getPublicId();
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState4() {
+		RegimenState expectedState = state4;
+		// 1 tetanus immunization, patient registered 0 minutes ago, message
+		// expected 1 minutes after last tetanus, which was 0 minutes ago
+		Integer numberOfTetanusObs = 1;
+		setPatientRegistration(patient, 1);
+		Date lastObsDate = getPreviousTetanusObsDate(0);
+		Date messageDate = getMessageDate(lastObsDate, 1);
+
+		String messageKey = ((ScheduleMessageCommand) expectedState
+				.getCommand()).getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) expectedState.getCommand())
+				.getPublicId();
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+		expect(
+				patientObsService.getLastObsDate(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(lastObsDate).anyTimes();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState5() {
+		RegimenState expectedState = state5;
+		// 1 tetanus immunization, patient registered 2 minutes ago, message
+		// expected 3 minutes after last tetanus, which was 2 minutes ago
+		Integer numberOfTetanusObs = 1;
+		setPatientRegistration(patient, 2);
+		Date lastObsDate = getPreviousTetanusObsDate(2);
+		Date messageDate = getMessageDate(lastObsDate, 3);
+
+		String messageKey = ((ScheduleMessageCommand) expectedState
+				.getCommand()).getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) expectedState.getCommand())
+				.getPublicId();
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+		expect(
+				patientObsService.getLastObsDate(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(lastObsDate).anyTimes();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState6() {
+		RegimenState expectedState = state6;
+		// 1 tetanus immunization, patient registered 4 minutes ago, message
+		// expected 6 minutes after last tetanus, which was 4 minutes ago
+		Integer numberOfTetanusObs = 1;
+		setPatientRegistration(patient, 4);
+		Date lastObsDate = getPreviousTetanusObsDate(4);
+		Date messageDate = getMessageDate(lastObsDate, 6);
+
+		String messageKey = ((ScheduleMessageCommand) expectedState
+				.getCommand()).getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) expectedState.getCommand())
+				.getPublicId();
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+		expect(
+				patientObsService.getLastObsDate(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(lastObsDate).anyTimes();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState7AllObs() {
+		RegimenState expectedState = state7;
+		// 2 tetanus immunizations, no messages expected
+		Integer numberOfTetanusObs = 2;
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	public void testState7NoObs() {
+		RegimenState expectedState = state7;
+		// No tetanus immunizations, patient registered 10 minutes ago, no
+		// messages expected
+		Integer numberOfTetanusObs = 0;
+		setPatientRegistration(patient, 10);
+
+		expect(
+				patientObsService.getNumberOfObs(patient, regimen
+						.getConceptName(), regimen.getConceptValue()))
+				.andReturn(numberOfTetanusObs).anyTimes();
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(expectedState.getName(), state.getName());
+	}
+
+	private Date setPatientRegistration(Patient patient, int minutesPrior) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.MINUTE, -minutesPrior);
+		calendar.add(Calendar.SECOND, -1);
+		patient.setDateCreated(calendar.getTime());
+		return calendar.getTime();
+	}
+
+	private Date getPreviousTetanusObsDate(int minutesPrior) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.MINUTE, -minutesPrior);
+		calendar.add(Calendar.SECOND, -1);
+		return calendar.getTime();
+	}
+
+	private Date getMessageDate(Date registrationDate, int minutesAfter) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(registrationDate);
+		calendar.add(Calendar.MINUTE, minutesAfter);
+		return calendar.getTime();
+	}
+}
---
+++ motech-omod/src/test/java/org/motech/event/TetanusInfoRegimenTest.java
@@ -0,0 +1,209 @@
+package org.motech.event;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.motech.event.impl.RegimenImpl;
+import org.motech.event.impl.RegimenStateImpl;
+import org.motech.event.impl.ScheduleMessageCommand;
+import org.motech.messaging.MessageScheduler;
+import org.openmrs.Patient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TetanusInfoRegimenTest extends TestCase {
+
+	ApplicationContext ctx;
+
+	Patient patient;
+	Integer patientId;
+
+	RegimenImpl regimen;
+	RegimenStateImpl state1;
+	RegimenStateImpl state2;
+	RegimenStateImpl state3;
+	RegimenStateImpl state4;
+	RegimenStateImpl state5;
+
+	MessageScheduler messageScheduler;
+	PatientObsService patientObsService;
+
+	@Override
+	protected void setUp() throws Exception {
+		patientId = 576;
+
+		patient = new Patient();
+		patient.setPatientId(patientId);
+
+		ctx = new ClassPathXmlApplicationContext("tetanus-info-regimen.xml");
+
+		regimen = (RegimenImpl) ctx.getBean("tetanusInfo");
+
+		state1 = (RegimenStateImpl) ctx.getBean("tetanusInfoState1");
+		state2 = (RegimenStateImpl) ctx.getBean("tetanusInfoState2");
+		state3 = (RegimenStateImpl) ctx.getBean("tetanusInfoState3");
+		state4 = (RegimenStateImpl) ctx.getBean("tetanusInfoState4");
+		state5 = (RegimenStateImpl) ctx.getBean("tetanusInfoState5");
+
+		patientObsService = createMock(PatientObsService.class);
+
+		state1.setPatientObsService(patientObsService);
+		state2.setPatientObsService(patientObsService);
+		state3.setPatientObsService(patientObsService);
+		state4.setPatientObsService(patientObsService);
+
+		messageScheduler = createMock(MessageScheduler.class);
+
+		((ScheduleMessageCommand) state1.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state2.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state3.getCommand())
+				.setMessageScheduler(messageScheduler);
+		((ScheduleMessageCommand) state4.getCommand())
+				.setMessageScheduler(messageScheduler);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		patient = null;
+		patientId = null;
+
+		regimen = null;
+		state1 = null;
+		state2 = null;
+		state3 = null;
+		state4 = null;
+		state5 = null;
+
+		ctx = null;
+
+		patientObsService = null;
+		messageScheduler = null;
+	}
+
+	public void testState1() {
+		// Patient registered 0 minutes ago, message expected in 1 minute
+		Date registrationDate = setPatientRegistration(patient, 0);
+		Date messageDate = getMessageDate(registrationDate, 1);
+
+		String messageKey = ((ScheduleMessageCommand) state1.getCommand())
+				.getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) state1.getCommand())
+				.getPublicId();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(state1.getName(), state.getName());
+	}
+
+	public void testState2() {
+		// Patient registered 1 minute ago, message expected 2 minutes after
+		// registration
+		Date registrationDate = setPatientRegistration(patient, 1);
+		Date messageDate = getMessageDate(registrationDate, 2);
+
+		String messageKey = ((ScheduleMessageCommand) state2.getCommand())
+				.getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) state2.getCommand())
+				.getPublicId();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(state2.getName(), state.getName());
+	}
+
+	public void testState3() {
+		// Patient registered 4 minute ago, message expected 5 minutes after
+		// registration
+		Date registrationDate = setPatientRegistration(patient, 4);
+		Date messageDate = getMessageDate(registrationDate, 5);
+
+		String messageKey = ((ScheduleMessageCommand) state3.getCommand())
+				.getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) state3.getCommand())
+				.getPublicId();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(state3.getName(), state.getName());
+	}
+
+	public void testState4() {
+		// Patient registered 5 minute ago, message expected 6 minutes after
+		// registration
+		Date registrationDate = setPatientRegistration(patient, 5);
+		Date messageDate = getMessageDate(registrationDate, 6);
+
+		String messageKey = ((ScheduleMessageCommand) state4.getCommand())
+				.getMessageKey();
+		Long publicId = ((ScheduleMessageCommand) state4.getCommand())
+				.getPublicId();
+
+		messageScheduler.scheduleMessage(messageKey, publicId, patientId,
+				messageDate);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(state4.getName(), state.getName());
+	}
+
+	public void testState5() {
+		// Patient registered 7 minute ago, no messages expected
+		setPatientRegistration(patient, 7);
+
+		replay(patientObsService, messageScheduler);
+
+		RegimenState state = regimen.determineState(patient);
+
+		verify(patientObsService, messageScheduler);
+
+		assertEquals(state5.getName(), state.getName());
+	}
+
+	private Date setPatientRegistration(Patient patient, int minutesPrior) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.MINUTE, -minutesPrior);
+		calendar.add(Calendar.SECOND, -1);
+		patient.setDateCreated(calendar.getTime());
+		return calendar.getTime();
+	}
+
+	private Date getMessageDate(Date registrationDate, int minutesAfter) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(registrationDate);
+		calendar.add(Calendar.MINUTE, minutesAfter);
+		return calendar.getTime();
+	}
+}
