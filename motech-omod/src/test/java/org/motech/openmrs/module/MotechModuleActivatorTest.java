0d4a18023f03c7f13cb70674b0690938b2a423ca
Matt Blanchette <mblanchette@usm.maine.edu>
2009-10-29 22:01:35 +0000

    Refs #121 Fix for build, Revert Activator Test change
    git-svn-id: https://motech.rcg.usm.maine.edu/svn/motech/trunk@302 6f28304a-7744-dd4f-b2a8-8a00c5622ee2

--- motech-omod/src/test/java/org/motech/openmrs/module/MotechModuleActivatorTest.java
+++ motech-omod/src/test/java/org/motech/openmrs/module/MotechModuleActivatorTest.java
@@ -3,15 +3,10 @@ package org.motech.openmrs.module;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openmrs.test.BaseContextSensitiveTest;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.openmrs.test.SkipBaseSetup;
-import org.springframework.test.context.ContextConfiguration;
 
-/* BaseContextSensitiveTest normally allows module spring configurations,
- * But this Test is limited to the OpenMRS core spring configuration, 
- * Since the module services are not available during the Activator */
-@ContextConfiguration(locations = { "classpath:applicationContext-service.xml" }, inheritLocations = false)
-public class MotechModuleActivatorTest extends BaseContextSensitiveTest {
+public class MotechModuleActivatorTest extends BaseModuleContextSensitiveTest {
 
 	MotechModuleActivator activator;
 
