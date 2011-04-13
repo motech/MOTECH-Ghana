8f210714506948fe95e63966f9f86c2bc43daacf
Subhrajit Roy <roy.subhrajit@gmail.com>
2011-04-13 19:11:11 +0530

    Adding DemoProgramFactory for demo program

---
+++ motech-server-omod/src/main/java/org/motechproject/server/svc/impl/DemoMessageProgramFactory.java
@@ -0,0 +1,17 @@
+package org.motechproject.server.svc.impl;
+
+import org.motechproject.server.event.MessageProgram;
+import org.motechproject.server.svc.MessageProgramFactory;
+
+public class DemoMessageProgramFactory implements MessageProgramFactory {
+
+    MessageProgram program ;
+
+    public MessageProgram program() {
+        return program ;
+    }
+
+    public void setProgram(MessageProgram program) {
+        this.program = program;
+    }
+}
