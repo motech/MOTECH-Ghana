35c2cd817fa8919808ef4f6aca98950ef8962a0c
Subhrajit Roy <roy.subhrajit@gmail.com>
2011-06-08 17:07:20 +0530

    Adding tests for defaulter alerts

---
+++ motech-server-omod/src/test/java/org/motechproject/server/svc/impl/DefaultedExpectedEncounterAlertMatcher.java
@@ -0,0 +1,24 @@
+package org.motechproject.server.svc.impl;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.easymock.IArgumentMatcher;
+import org.junit.Ignore;
+import org.motechproject.server.model.DefaultedExpectedEncounterAlert;
+
+@Ignore
+public class DefaultedExpectedEncounterAlertMatcher implements IArgumentMatcher {
+    private DefaultedExpectedEncounterAlert expected;
+
+    public DefaultedExpectedEncounterAlertMatcher(DefaultedExpectedEncounterAlert expectedEncounterAlert) {
+        this.expected = expectedEncounterAlert;
+    }
+
+    public boolean matches(Object argument) {
+        DefaultedExpectedEncounterAlert actual = (DefaultedExpectedEncounterAlert) argument;
+        return EqualsBuilder.reflectionEquals(expected, actual);
+    }
+
+    public void appendTo(StringBuffer buffer) {
+        buffer.append("Expected ").append(expected);
+    }
+}
---
+++ motech-server-omod/src/test/java/org/motechproject/server/svc/impl/DefaultedExpectedObsAlertMatcher.java
@@ -0,0 +1,23 @@
+package org.motechproject.server.svc.impl;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.easymock.IArgumentMatcher;
+import org.motechproject.server.model.DefaultedExpectedObsAlert;
+
+public class DefaultedExpectedObsAlertMatcher implements IArgumentMatcher{
+
+    private DefaultedExpectedObsAlert expected;
+
+    public DefaultedExpectedObsAlertMatcher(DefaultedExpectedObsAlert expectedObsAlert) {
+        this.expected = expectedObsAlert;
+    }
+
+    public boolean matches(Object argument) {
+        DefaultedExpectedObsAlert actual = (DefaultedExpectedObsAlert) argument;
+        return EqualsBuilder.reflectionEquals(expected, actual);
+    }
+
+    public void appendTo(StringBuffer buffer) {
+        buffer.append("Expected ").append(expected);
+    }
+}
